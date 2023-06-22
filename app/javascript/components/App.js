27c488ae476c891981fd0eeba2b71a2ceeaab886
kensteph <kenderromain@gmail.com>
2023-06-22 14:59:27 -0400

    Create the redux store.

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Greetings from './Greetings';
+import { Provider } from 'react-redux';
+
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <Greetings/>
   },
 ]);
 
